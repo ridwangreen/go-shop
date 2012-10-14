e4b8b09084e2412fe9649af1488f6a5876fc1603
rtk1865@g.rit.edu <rtk1865@g.rit.edu@6a267f4d-1c5d-f686-03b5-69e9f5e1aa62>
2012-10-14 23:31:01 +0000

    First draft of goSHOP UI

--- trunk/goSHOP/src/com/example/goshop/GoShopActivity.java
+++ trunk/goSHOP/src/com/example/goshop/GoShopActivity.java
@@ -1,8 +1,9 @@
 package com.example.goshop;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class GoShopActivity extends Activity {
 
@@ -10,7 +11,11 @@ public class GoShopActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_shop);
+        
+        //String[] categories = getResources().getStringArray(R.array.category_array);
+                
     }
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
