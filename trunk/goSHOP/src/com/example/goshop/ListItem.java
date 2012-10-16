8de6f580d1d4bf3c28007ec5d218ce0dd96018aa
cjl1750@rit.edu <cjl1750@rit.edu@6a267f4d-1c5d-f686-03b5-69e9f5e1aa62>
2012-10-16 23:29:58 +0000

    Abstracted parts of item and Category into list item.

--- trunk/goSHOP/src/com/example/goshop/ListItem.java
+++ trunk/goSHOP/src/com/example/goshop/ListItem.java
@@ -1,6 +1,17 @@
 package com.example.goshop;
 
 public abstract class ListItem {
-
+	private String name;
 	
+	public ListItem(String name) {
+		this.name=name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void editName(String newName) {
+		this.name = newName;
+	}
 }
