103488d20220ef3da7d94286f0247193b3055325
cjl1750@rit.edu <cjl1750@rit.edu@6a267f4d-1c5d-f686-03b5-69e9f5e1aa62>
2012-10-16 14:52:53 +0000

    Added base skeleton file for ItemManager.java. Also testing SVN

---
+++ goSHOP/src/com/example/goshop/ItemManager.java
@@ -0,0 +1,33 @@
+/**ItemManager.java
+ * 
+ */
+package com.example.goshop;
+
+/**
+ * This class will be responsible for managing the list of items.
+ * 
+ * @author Charles Lander (cjl1750@rit.edu)
+ *
+ */
+public class ItemManager {
+	
+	/**
+	 * @param item the item to add
+	 * @return if the item was added or not
+	 */
+	public boolean addItem(Item item) {
+		//TODO Implement
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param item to be removed
+	 * @return if the item was removed or not
+	 */
+	public boolean removeItem(Item item ) {
+		//TODO Implement
+		return false;
+	}
+
+}
