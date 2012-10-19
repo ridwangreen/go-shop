418741e72f201fd298b7e10f9244c3a0945dd83b
cjl1750@rit.edu <cjl1750@rit.edu@6a267f4d-1c5d-f686-03b5-69e9f5e1aa62>
2012-10-19 00:19:16 +0000

    We can now add categories... Yes we can...

--- goSHOP/src/com/example/goshop/GoShopActivity.java
+++ goSHOP/src/com/example/goshop/GoShopActivity.java
@@ -2,6 +2,7 @@ package com.example.goshop;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,12 +12,13 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.data.Category;
 import com.example.data.DataModelInterface;
 import com.example.data.ItemManager;
 
 
 public class GoShopActivity extends Activity {
-	
+	private static int ADD_CATEGORY_REQUEST_CODE = 12354;
 	private ShoppingListAdapter shoppingListAdapter;
 	private CategoryListAdapter categoryListAdapter;
 	
@@ -53,13 +55,27 @@ public class GoShopActivity extends Activity {
         switch (item.getItemId()) {
             case R.id.addCategory1:
             	Intent intent = new Intent(this, AddCategoryActivity.class);
-            	startActivity(intent);
+            	startActivityForResult(intent, ADD_CATEGORY_REQUEST_CODE);
             	
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("Resulting Activities");
+    	// If the request went well (OK) and the request was PICK_CONTACT_REQUEST
+        if (resultCode == RESULT_OK && requestCode == ADD_CATEGORY_REQUEST_CODE) {
+        	String newCategoryName = data.getStringExtra(AddCategoryActivity.ADDED_CATEGORY_ID);
+        	shoppingListAdapter.addCategory(newCategoryName, Color.BLACK);
+        	categoryListAdapter.add(new Category(newCategoryName, Color.BLACK));
+
+        }
+    }
+    
    public void addQuickItem(View view){
     	EditText editText = (EditText) findViewById(R.id.quick_add_text);
     	Spinner categoryList = (Spinner) findViewById(R.id.category_list);
