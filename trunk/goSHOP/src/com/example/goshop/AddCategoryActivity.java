67a3e5d51fb7db5564b0f19b61844c6899ea9659
cjl1750@rit.edu <cjl1750@rit.edu@6a267f4d-1c5d-f686-03b5-69e9f5e1aa62>
2012-10-19 00:19:16 +0000

    We can now add categories... Yes we can...

--- trunk/goSHOP/src/com/example/goshop/AddCategoryActivity.java
+++ trunk/goSHOP/src/com/example/goshop/AddCategoryActivity.java
@@ -1,10 +1,13 @@
 package com.example.goshop;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddCategoryActivity extends Activity{
-	
+	public static String ADDED_CATEGORY_ID = "addedcategory";
 	 @Override
 	 public void onCreate(Bundle savedInstanceState) {
 		 super.onCreate(savedInstanceState);
@@ -12,7 +15,15 @@ public class AddCategoryActivity extends Activity{
 	 }
 	 @Override 
 	 public void onPause() {
-		 
+		 super.onPause();
+	 }
+	 public void addCategory(View view){
+		 EditText editText = (EditText) findViewById(R.id.editText1);
+		 System.out.println("Edit Text: " + editText.getText());
+		 Intent intent = getIntent();
+		 intent.putExtra(ADDED_CATEGORY_ID, editText.getText().toString());
+		 setResult(RESULT_OK, intent);
+		 finish();
 	 }
 }
 
